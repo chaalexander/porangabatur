6c92736c3703b2e648f71a2f3736e7fdcd7f96ef
charlenne alexander <charlennep@gmail.com>
2020-10-06 15:11:19 -0500

    added some features

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,27 +1,19 @@
 import React from "react";
-import profilePic from "../assets/images/profile.png";
 
 function AboutContent() {
   return (
     <>
       <div className="container" id="about">
         <div className="media w3-black">
-          <img src={profilePic} className="img" alt="Profile" />
           <div className="media-body">
             <p className="aboutmeBig">
-              Brazilian full Stack Web Developer with a background in sales,
-              fashion and customer service providing strong problem-solving
-              skills with a keen eye for aesthetics and presentation. An
-              excellent communicator and collaborator who works well in
-              team-based projects and independently with very strong
-              interpersonal skills. Effectively leverage interest in solving
-              complex problems by contributing as a part of a fast paced,
-              quality-driven team.
+              Agencia de turismo localizada no Brasil e Estados Unidos. Focada
+              em pacotes de viagens nacionais e internacionais.
+              Clique em cidades na barra de navegacao para ver algumas opcoes.
             </p>
             <p className="aboutmeSml">
-              Brazilian full Stack Web Developer with a background in sales,
-              fashion and customer service providing strong problem-solving
-              skills with a keen eye for aesthetics and presentation.
+              Agencia de turismo localizada no Brasil e Estados Unidos. Focada
+              em pacotes de viagens nacionais e internacionais.
             </p>
           </div>
         </div>
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Contact from "../Contact";
+// import Contact from "../Contact";
 import AboutContent from "../AboutContent";
 
 function About() {
   return (
     <>
       <AboutContent />
-      <Contact />
+      {/* <Contact /> */}
     </>
   );
 }
